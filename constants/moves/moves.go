e926509c314325d2d8c0b78e63751baaea4c7051
nedlir <neliran2@telem.openu.ac.il>
2025-05-23 22:37:48 +0200

    removed redundant moves for king

--- constants/moves/moves.go
+++ constants/moves/moves.go
@@ -1,15 +1,12 @@
 package moves
 
-// TODO: need to check if some of these are redundant.
 // logic of king move will be, if the current move is same like previous move
 // then it is an assistance move.
 // will have to determine skip move based on how many squares are left.
 // need to verify this approach.
 const (
-	Kc8 KingMove = "Kc8"
 	Kd8 KingMove = "Kd8"
 	Ke8 KingMove = "Ke8"
 	Kf8 KingMove = "Kf8"
 	Kg8 KingMove = "Kg8"
-	Kh8 KingMove = "Kh8"
 )
