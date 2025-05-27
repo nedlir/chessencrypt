8060769790cf5c813eed6201849d251e1f2c6a64
nedlir <neliran2@telem.openu.ac.il>
2025-05-27 07:14:06 +0200

    changed fen to minimize moves allowed for black

--- chess/fen/fen.go
+++ chess/fen/fen.go
@@ -2,8 +2,8 @@ package fen
 
 // FEN string for initial encoding based on whether a6's bit is 0 or 1
 const (
-	FENZeroA6     = "K1k2q2/1ppppppr/Q7/8/8/8/8/8 b - - 0 1"
-	FENOneA6      = "2k2q2/Kppppppr/Q7/8/8/8/8/8 b - - 0 1"
-	WhiteKingOnA8 = "K"
-	WhiteKingOnA7 = "2"
+	FENZeroA6     = "3k1q2/Kppppppr/Q7/8/8/8/8/8 b - - 0 1"
+	FENOneA6      = "K2k1q2/1ppppppr/Q7/8/8/8/8/8 b - - 0 1"
+	WhiteKingOnA8 = "3"
+	WhiteKingOnA7 = "K"
 )
--- chess/fen/types.go
+++
@@ -1,3 +0,0 @@
-package fen
-
-type FEN string
