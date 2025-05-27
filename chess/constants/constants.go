934249143ab25f9a3c6b6267a9229a4e52fc4b1e
nedlir <neliran2@telem.openu.ac.il>
2025-05-27 07:15:58 +0200

    square positions for fast lookup

--- chess/constants/constants.go
+++ chess/constants/constants.go
@@ -1,17 +1,10 @@
 package constants
 
 type (
-	Color  string
-	Square string
-	Row    int
-	Column int
+	Color string
 )
 
 const (
-	BLACK                       Color  = "black"
-	WHITE                       Color  = "black"
-	BLACK_QUEEN_STARTING_SQUARE Square = "Qf8"
-	WHITE_QUEEN_STARTING_SQUARE Square = "Qa6"
-	// TODO: need to think later how to represent the start of the game for black.
-	WHITE_QUEEN_MOVE_ZERO Square = "..."
+	BLACK Color = "black"
+	WHITE Color = "white"
 )
