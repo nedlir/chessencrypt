207c2a703a5a73be9a3568da0089abd433203ea9
nedlir <neliran2@telem.openu.ac.il>
2025-06-02 21:50:09 +0200

    pgn decoding scaffold

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 
 	matrix := []byte{
-		0b01011010,
+		0b11011010,
 		// 0b10100101,
 		// 0b10110110,
 		// 0b00000001,
@@ -19,21 +19,16 @@ func main() {
 		0b00101100,
 		0b01101100,
 	}
-	matrix2 := []byte{
-		0b01011010,
-		0b10100101,
-		0b10110110,
-		0b00000001,
-		0b00101100,
-		0b01101100,
-	}
 
+	fmt.Println("1")
 	encoder := pgn.NewPGNEncoder()
 
 	pgn1 := encoder.BytesToPgn(matrix, 1)
 
+	fmt.Println("2")
 	fmt.Printf("PGN:\n %v", pgn1)
 
-	fmt.Printf("PGN:\n %v", encoder.BytesToPgn(matrix2, 2))
+	decoder := pgn.NewPGNDecoder()
+	decoder.PGNToBytes(pgn1)
 
 }
